5f070c8b22f17998cdcfa49d0b4bb7164b8f0c06
naveennvm4 <naveennvm4@gmail.com>
2021-02-26 09:31:50 +0530

    Ability to choose a valid cells to make a move

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,42 +1,61 @@
 package package1;
+
 import java.util.Scanner;
 
 public class TicTacToe {
-	public static void board() {
+	public char[] board() {
 		char[] board = new char[10];
-		for (int i=0; i<10; i++) {
-			board[i]=' ';
-			System.out.println(board[i]);
+		for(int i = 1; i < board.length; i++) {
+			board[i] = ' ';
 		}
-	}
-	public static void entry() {
-		Scanner user = new Scanner(System.in);
-		System.out.println("Enter a choice x or o");
-		char player = user.next().charAt(0);
-		char computer = ' ';
-		if(player == 'x' || player == 'X') {
-			computer = 'o';
-		}
-		else if (player == 'O' || player == 'o') {
-			computer = 'x';
-		}
-		else {
-			System.out.println("Invalid");
-		}
-		System.out.println("Player is " + player + " and Computer is " + computer);
-	}
-	public static void showBoard() {
-		System.out.println("  |  |  ");
-		System.out.println("--------");
-		System.out.println("  |  |  ");
-		System.out.println("--------");
-		System.out.println("  |  |  ");
+		return board;
 	}
 
-	public static void main(String args[]) {
-		System.out.println("Welcome to TicTacToeGame");
-		board();
-		entry();
-		showBoard();
- 	}
+	public char entry() {
+		char player, computer;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter x for chhosing cross and enter o for choosing circle");
+		char selection = input.next().charAt(0);
+		if (selection == 'x') {
+			player = 'x';
+			computer = 'o';
+		}
+		else {
+			player = 'o';
+			computer = 'x';
+		}
+		return player;
+	}
+
+	public void showBoard(char[] board) {
+		System.out.println(board[1] + "  | " + board[2] + " | " + board[3]);
+		System.out.println("--------------");
+		System.out.println(board[4] + "  | " + board[5] + " | " + board[6]);
+		System.out.println("--------------");
+		System.out.println(board[7] + "  | " + board[8] + " | " + board[9]);
+	}
+
+	public void userInput(char[] board, char player) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number between 1 to 9");
+		int user_input = input.nextInt();
+		if (user_input < 10 && user_input > 0) {
+			if (board[user_input] == ' ') {
+				System.out.println("Place is available");
+			}
+			else
+				System.out.println("Place is already taken");
+		}
+		else
+			System.out.println("Invalid Input");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to tictactoe game");
+		TicTacToe game = new TicTacToe();
+		char[] board = game.board();
+		char player = game.entry();
+		game.showBoard(board);
+		game.userInput(board, player);
+	}
 }
