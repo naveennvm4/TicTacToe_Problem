1aa7cc670f312fe4c36c397c0a79d9040743a9d1
naveennvm4 <naveennvm4@gmail.com>
2021-02-26 08:45:55 +0530

    Create an empty board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,10 @@
-public class TicTacToe{
-	public static void main(String args[]){
-		System.out.println("Welcome to TicTacToeGame");
+package package1;
+public class TicTacToe {
+	public static void main(String args[]) {
+		char[] board = new char[10];
+		for (int i=0; i<10; i++) {
+			board[i]=' ';
+			System.out.println(board[i]);
+		}
 	}
 }
